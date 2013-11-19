4c129944eda605c3e989f9d7a52d744b84ac496e
ZacharyKamerling <ZacharyKamerling@gmail.com>
2013-11-19 10:23:38 -0800

    Network Testing
    I got it working!

--- server_src/GameNetwork.hs
+++ server_src/GameNetwork.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE ScopedTypeVariables, OverloadedStrings #-}
 
-module GameNetwork where
+module GameNetwork (tcpGameServer,PlayerConnection) where
 
 import qualified Data.Vector.Unboxed as V
 import qualified Data.ByteString.Lazy as B
@@ -8,13 +8,12 @@ import qualified Data.Vector.Unboxed.Mutable as M
 import Network.Socket hiding (send, sendTo, recv, recvFrom)
 import Network.Socket.ByteString (send)
 import Network.Socket.ByteString.Lazy
-import Control.Concurrent.STM.TVar
-import Control.Monad (forever)
 import Data.Binary (decodeOrFail,Binary,get,put,Get)
 import Control.Concurrent.STM.TVar
+import Control.Concurrent (forkIO,killThread,myThreadId,ThreadId)
+import Control.Concurrent.MVar (putMVar,takeMVar,newEmptyMVar)
 import Control.Monad.STM (atomically)
 import System.Timeout (timeout)
-import Control.Concurrent (forkIO)
 import Data
 
 type Name = String
@@ -29,33 +28,42 @@ instance Binary HelloMessage where
     put = undefined
 
 data PlayerConnection = PlayerConnection Name PlayerStatus Socket Socket SockAddr
-data ServerState = ServerState (V.Vector Int) [PlayerConnection]
+data ServerState = ServerState 
+    { serverState_teamCounts :: (V.Vector Int)
+    , serverState_playerCons :: [PlayerConnection]
+    } 
 
-tcpGameServer :: V.Vector Int -> IO ()
+tcpGameServer :: V.Vector Int -> IO [PlayerConnection]
 tcpGameServer teamCounts = withSocketsDo $ do
-    let addrFamily = if isSupportedFamily AF_INET6 then AF_INET6 else AF_INET
+    sync <- newEmptyMVar
+    -- let addrFamily = if isSupportedFamily AF_INET6 then AF_INET6 else AF_INET
     localAddr <- fmap head $ getAddrInfo (Just (defaultHints {addrFlags = [AI_PASSIVE]})) 
-                                         (Just "Scrounger") 
-                                         (Just "3000")
-    sock <- socket addrFamily Stream defaultProtocol
+                                         Nothing (Just "3000")
+    sock <- socket (addrFamily localAddr) Stream defaultProtocol
     bind sock (addrAddress localAddr)
     serverStateVar <- newTVarIO $ ServerState teamCounts []
     -- Only accept as many connections as there are potential players
     listen sock $ V.foldl (+) 0 teamCounts
     -- Accept a single player
-    let acceptPlayer :: (Socket,SockAddr) -> IO ()
-        acceptPlayer (sockTCP,addr) = do
-            bind sockTCP addr
+    let acceptPlayer :: (Socket,SockAddr) -> ThreadId -> IO ()
+        acceptPlayer (sockTCP,addr) acceptLoop = do
+            -- putStrLn $ "Binding TCP socket to " ++ show addr
+            -- bind sockTCP addr
             -- Try to validate player. Give up in 5 seconds.
+            putStrLn "Validating connection"
             accepted <- timeout 5000000 $ do
                 msg <- recv sockTCP 512
                 case decodeOrFail msg of
                     Left _ -> putStrLn "Somebody didn't have a multi-pass"
                     Right (_,_, HelloMessage name team) -> do
                         -- Get next connection
-                        sockUDP <- socket addrFamily Datagram defaultProtocol
+                        putStrLn "Making UDP socket"
+                        sockUDP <- socket (addrFamily localAddr) Datagram defaultProtocol
+                        putStrLn $ "Binding UDP socket to " ++ show (addrAddress localAddr)
                         bind sockUDP (addrAddress localAddr)
+                        putStrLn $ "Connecting UDP socket to " ++ show addr
                         connect sockUDP addr
+                        putStrLn $ "Checking for team slots"
                         slotExisted <- atomically $ do
                             (ServerState teamCounts playerList) <- readTVar serverStateVar
                             case teamCounts V.!? team of
@@ -67,17 +75,26 @@ tcpGameServer teamCounts = withSocketsDo $ do
                                         (PlayerConnection name Playing sockTCP sockUDP addr : playerList))
                                         >> return True
                                     else return False
-                        if slotExisted
-                        then putStrLn $ name ++ " joined team " ++ show team
-                        else putStrLn $ name ++ " couldn't join team " ++ show team
+                        if not slotExisted
+                        then putStrLn $ name ++ " couldn't join team " ++ show team
+                        else do 
+                            putStrLn $ name ++ " joined team " ++ show team 
+                            if V.foldl (+) 0 teamCounts /= 0
+                            then return ()
+                            else do
+                                putStrLn "Starting game..."
+                                atomically (readTVar serverStateVar) >>= putMVar sync . serverState_playerCons
+                                killThread acceptLoop
             case accepted of
                 Just __ -> return ()
                 Nothing -> return ()
         -- Loop of accepting new players
         acceptPlayers :: IO ()
         acceptPlayers = do
+            putStrLn "Waiting for connection"
             connection <- accept sock
             putStrLn $ "Accepted connection from " ++ show (snd connection)
-            forkIO $ acceptPlayer connection
+            myThreadId >>= forkIO . acceptPlayer connection
             acceptPlayers
-    acceptPlayers
+    forkIO acceptPlayers
+    takeMVar sync
--- server_src/Main.hs
+++ server_src/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import qualified Data.Vector.Unboxed as V (fromList)
 import Data.Binary (decodeOrFail)
 import Data.Binary.Get (ByteOffset)
 import Data.ByteString.Lazy (ByteString)
@@ -9,33 +10,51 @@ import Control.Concurrent (forkIO, threadDelay, myThreadId)
 import GHC.Conc.Sync (ThreadId)
 import System.Environment (getArgs)
 import System.Timeout (timeout)
+import Control.Concurrent.MVar (newEmptyMVar,takeMVar)
+import Network.Info (getNetworkInterfaces,mac)
+import Network.Socket hiding (send, sendTo, recv, recvFrom)
+import Network.Socket.ByteString (send)
+import Network.Socket.ByteString.Lazy
 import Control.Concurrent.STM.TVar 
-	( TVar
-	, modifyTVar
-	, readTVar
-	, newTVarIO 
-	, writeTVar )
+    ( TVar
+    , modifyTVar
+    , readTVar
+    , newTVarIO 
+    , writeTVar )
 import Data.Time.Clock 
-	( UTCTime
-	, NominalDiffTime
-	, diffUTCTime
-	, getCurrentTime
-	, picosecondsToDiffTime )
+    ( UTCTime
+    , NominalDiffTime
+    , diffUTCTime
+    , getCurrentTime
+    , picosecondsToDiffTime )
+-- Local code
 import Data
+import GameNetwork
 
-mainy = do 
-	numPlayers <- fmap (read . head) getArgs :: IO Int
-	t_ss <- newTVarIO $ Server 4 [] []
-	forkIO $ runServer "0.0.0.0" 8666 $ connectPlayers t_ss
-	time <- getCurrentTime
-	loop 10 time 1
+main = do 
+    getNetworkInterfaces >>= print . mac . head
+    -- numPlayers <- fmap (read . head) getArgs :: IO Int
+    -- teamCounts <- fmap read getLine
+    let teamCounts = [1]
+    playerCons <- tcpGameServer $ V.fromList teamCounts
+    -- Start main loop
+    getCurrentTime >>= loop 10 1
 
-loop :: Integer -> UTCTime -> Integer -> IO ()
-loop fps time steps = do
-	putStrLn $ show steps
-	timeNow <- getCurrentTime
-	threadDelay $ max 0 $ fromIntegral $ 
-		(steps * 1000000 `div` fps) - 
-		ceiling (diffUTCTime timeNow time * fromInteger 1000000)
-	loop fps time (steps + 1)
+loop :: Integer -> Integer -> UTCTime -> IO ()
+loop fps steps time = do
+    putStrLn $ show steps
+    timeNow <- getCurrentTime
+    threadDelay $ max 0 $ fromIntegral $ 
+        (steps * 1000000 `div` fps) - 
+        ceiling (diffUTCTime timeNow time * fromInteger 1000000)
+    loop fps (steps + 1) time 
 
+test :: IO ()
+test = withSocketsDo $ do 
+    putStrLn "Enter Server Address"
+    serverAddress <- getLine
+    addrinfos <- getAddrInfo Nothing (Just serverAddress) (Just "3000")
+    let serveraddr = head addrinfos
+    sock <- socket (addrFamily serveraddr) Stream defaultProtocol
+    connect sock (addrAddress serveraddr)
+    sClose sock
