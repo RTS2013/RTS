7736bb595b81a30861bfaa19f513006f43f4cd47
ZacharyKamerling <ZacharyKamerling@gmail.com>
2014-03-21 17:48:45 -0700

    KD-Tree Overhaul
    Added error catching and cleaner API.

--- server/src/KDT.hs
+++ server/src/KDT.hs
@@ -4,6 +4,7 @@ module KDT
 ( KDT
 , empty
 , make
+, makeFrom
 , nearby
 , nearbyMatching
 ) where
@@ -12,130 +13,143 @@ import           Data.Set (Set)
 import qualified Data.Set as Set
 import           Control.Parallel.Strategies (rpar, rseq, runEval)
 
+data KDTree n a = KDTree [a -> n] !(a -> n) !(KDT n a)
+
 data KDT n a = Fork !n !(KDT n a) !(KDT n a) | Leaf !(Set a)
 
 instance (Show n, Show a) => Show (KDT n a) where
     show = toStr 4
         where
-        toStr i (Fork n l r) = "Fork " 
-                            ++ show n 
-                            ++ "\n" 
-                            ++ replicate i ' ' 
-                            ++ toStr (i+4) l 
-                            ++ "\n" ++ replicate i ' ' 
-                            ++ toStr (i+4) r
-        toStr _ (Leaf xs) = "Leaf " ++ show xs
+            toStr i (Fork n l r) = "Fork " 
+                                ++ show n 
+                                ++ "\n" 
+                                ++ replicate i ' ' 
+                                ++ toStr (i+4) l 
+                                ++ "\n" ++ replicate i ' ' 
+                                ++ toStr (i+4) r
+            toStr _ (Leaf xs) = "Leaf " ++ show xs
 
-empty :: (Floating n, Ord n) => KDT n a
-empty = Leaf Set.empty
+empty :: (Floating n, Ord n) => (a -> n) -> [a -> n] -> KDTree n a
+empty _ [] = error "Cannot make 0 dimensional KD-Tree."
+empty radius getters = KDTree getters radius $ Leaf Set.empty
 
-make :: (Floating n, Ord n, Ord a) => (a -> n) -> [a -> n] -> [a] -> KDT n a
-make _ [] _ = Leaf Set.empty
-make radius getters entities = mkKDT (2::Int) 
-                                     (cycle $ tail getters) 
-                                     (split (mean 0 0 (head getters) entities) 
-                                            (head getters) 
-                                            entities 
-                                            [] [] 0 0 0 0) 
-                                     (-1) 
-                                     (-1)
+make :: (Floating n, Ord n, Ord a) => (a -> n) -> [a -> n] -> [a] -> KDTree n a
+make _ [] _ = error "Cannot make 0 dimensional KD-Tree."
+make radius getters entities = KDTree getters radius $ 
+        mkKDT (2::Int) 
+              (cycle $ tail getters) 
+              (split (mean 0 0 (head getters) entities) 
+                     (head getters) 
+                     entities 
+                     [] [] 0 0 0 0) 
+              (-1) 
+              (-1)
     where
-    mean n a _ [    ] = a / n
-    mean n a f (x:xs) = mean (n + 1) (a + f x) f xs
+        mean n a _ [    ] = a / n
+        mean n a f (x:xs) = mean (n + 1) (a + f x) f xs
 
-    split _ _ [] ls rs la ln ra rn = (ls,la,ln,rs,ra,rn)
-    split avg f (x:xs) ls rs la ln ra rn = 
-        let v = f x in
-        if v == avg
-        then split avg f xs (x:ls) (x:rs) (la+v) (ln+1) (ra+v) (rn+1) 
-        else if v < avg 
-             then if v + radius x >= avg 
-                  then split avg f xs (x:ls) (x:rs) (la+v) (ln+1) (ra+v) (rn+1) 
-                  else split avg f xs (x:ls) rs (la+v) (ln+1) ra rn
-             else if v - radius x <= avg
-                  then split avg f xs (x:ls) (x:rs) (la+v) (ln+1) (ra+v) (rn+1) 
-                  else split avg f xs ls (x:rs) la ln (ra+v) (rn+1)
+        split _ _ [] ls rs la ln ra rn = (ls,la,ln,rs,ra,rn)
+        split avg f (x:xs) ls rs la ln ra rn = 
+            let v = f x in
+            if v == avg
+            then split avg f xs (x:ls) (x:rs) (la+v) (ln+1) (ra+v) (rn+1) 
+            else if v < avg 
+                 then if v + radius x >= avg 
+                      then split avg f xs (x:ls) (x:rs) (la+v) (ln+1) (ra+v) (rn+1) 
+                      else split avg f xs (x:ls) rs (la+v) (ln+1) ra rn
+                 else if v - radius x <= avg
+                      then split avg f xs (x:ls) (x:rs) (la+v) (ln+1) (ra+v) (rn+1) 
+                      else split avg f xs ls (x:rs) la ln (ra+v) (rn+1)
 
-    mkKDT 0 (f:fs) (ls,la,ln,rs,ra,rn) pln prn =
-        if ln <= 1
-        then if rn <= 1 
-             then if ln == 1 && rn == 1
-                  then if ls == rs
-                       then Leaf $ Set.fromList ls
-                       else Fork ((la+ra)/2) (Leaf $ Set.fromList ls) (Leaf $ Set.fromList rs)
-                  else if ln == 0
-                       then Leaf $ Set.fromList rs
-                       else Leaf $ Set.fromList ls
-             else let lKDT = (mkKDT 0 fs (split (la/ln) f ls [] [] 0 0 0 0) ln rn)
-                      rKDT = (mkKDT 0 fs (split (ra/rn) f rs [] [] 0 0 0 0) ln rn) in
-                  Fork ((la+ra) / (ln+rn)) lKDT rKDT
-        else if rn == 0
-             then Leaf $ Set.fromList ls
-             else if pln == ln && prn == rn 
-                  then if ls == rs
-                       then Leaf $ Set.fromList ls
-                       else Fork ((la+ra)/2) (Leaf $ Set.fromList ls) (Leaf $ Set.fromList rs)
-                  else let lKDT = (mkKDT 0 fs (split (la/ln) f ls [] [] 0 0 0 0) ln rn)
-                           rKDT = (mkKDT 0 fs (split (ra/rn) f rs [] [] 0 0 0 0) ln rn) in
-                        Fork ((la+ra) / (ln+rn)) lKDT rKDT
-    mkKDT n (f:fs) (ls,la,ln,rs,ra,rn) pln prn =
-        if ln <= 1
-        then if rn <= 1 
-             then if ln == 1 && rn == 1
-                  then if ls == rs
-                       then Leaf $ Set.fromList ls
-                       else Fork ((la+ra)/2) (Leaf $ Set.fromList ls) (Leaf $ Set.fromList rs)
-                  else if ln == 0
-                       then Leaf $ Set.fromList rs
-                       else Leaf $ Set.fromList ls
-             else runEval $ do
-                      lKDT <- rpar (mkKDT (n-1) fs (split (la/ln) f ls [] [] 0 0 0 0) ln rn)
-                      rKDT <- rseq (mkKDT (n-1) fs (split (ra/rn) f rs [] [] 0 0 0 0) ln rn)
-                      _ <- rseq lKDT
-                      return $ Fork ((la+ra) / (ln+rn)) lKDT rKDT
-        else if rn == 0
-             then Leaf $ Set.fromList ls
-             else if pln == ln && prn == rn 
-                  then if ls == rs
-                       then Leaf $ Set.fromList ls
-                       else Fork ((la+ra)/2) (Leaf $ Set.fromList ls) (Leaf $ Set.fromList rs)
-                  else runEval $ do
+        mkKDT 0 (f:fs) (ls,la,ln,rs,ra,rn) pln prn =
+            if ln <= 1
+            then if rn <= 1 
+                 then if ln == 1 && rn == 1
+                      then if ls == rs
+                           then Leaf $ Set.fromList ls
+                           else Fork ((la+ra)/2) (Leaf $ Set.fromList ls) (Leaf $ Set.fromList rs)
+                      else if ln == 0
+                           then Leaf $ Set.fromList rs
+                           else Leaf $ Set.fromList ls
+                 else let lKDT = (mkKDT 0 fs (split (la/ln) f ls [] [] 0 0 0 0) ln rn)
+                          rKDT = (mkKDT 0 fs (split (ra/rn) f rs [] [] 0 0 0 0) ln rn) in
+                      Fork ((la+ra) / (ln+rn)) lKDT rKDT
+            else if rn == 0
+                 then Leaf $ Set.fromList ls
+                 else if pln == ln && prn == rn 
+                      then if ls == rs
+                           then Leaf $ Set.fromList ls
+                           else Fork ((la+ra)/2) (Leaf $ Set.fromList ls) (Leaf $ Set.fromList rs)
+                      else let lKDT = (mkKDT 0 fs (split (la/ln) f ls [] [] 0 0 0 0) ln rn)
+                               rKDT = (mkKDT 0 fs (split (ra/rn) f rs [] [] 0 0 0 0) ln rn) in
+                            Fork ((la+ra) / (ln+rn)) lKDT rKDT
+        mkKDT n (f:fs) (ls,la,ln,rs,ra,rn) pln prn =
+            if ln <= 1
+            then if rn <= 1 
+                 then if ln == 1 && rn == 1
+                      then if ls == rs
+                           then Leaf $ Set.fromList ls
+                           else Fork ((la+ra)/2) (Leaf $ Set.fromList ls) (Leaf $ Set.fromList rs)
+                      else if ln == 0
+                           then Leaf $ Set.fromList rs
+                           else Leaf $ Set.fromList ls
+                 else runEval $ do
                           lKDT <- rpar (mkKDT (n-1) fs (split (la/ln) f ls [] [] 0 0 0 0) ln rn)
                           rKDT <- rseq (mkKDT (n-1) fs (split (ra/rn) f rs [] [] 0 0 0 0) ln rn)
-                          _ <- rseq lKDT
+                          _    <- rseq lKDT
                           return $ Fork ((la+ra) / (ln+rn)) lKDT rKDT
-    mkKDT _ _ _ _ _ = Leaf Set.empty
+            else if rn == 0
+                 then Leaf $ Set.fromList ls
+                 else if pln == ln && prn == rn 
+                      then if ls == rs
+                           then Leaf $ Set.fromList ls
+                           else Fork ((la+ra)/2) (Leaf $ Set.fromList ls) (Leaf $ Set.fromList rs)
+                      else runEval $ do
+                              lKDT <- rpar (mkKDT (n-1) fs (split (la/ln) f ls [] [] 0 0 0 0) ln rn)
+                              rKDT <- rseq (mkKDT (n-1) fs (split (ra/rn) f rs [] [] 0 0 0 0) ln rn)
+                              _    <- rseq lKDT
+                              return $ Fork ((la+ra) / (ln+rn)) lKDT rKDT
+        mkKDT _ _ _ _ _ = Leaf Set.empty
 
-nearby :: (Floating n, Ord n, Ord a) => KDT n a -> [a -> n] -> [n] -> n -> Set a
-nearby kdt getters ns r = searchKDT leftRightChecks
-    where
-    dims = length ns
-    fns = zip getters ns
-    rangeFilter a = sum (map (\(f,n) -> (f a-n)^(2::Int)) fns) <= r^dims
-    leftRightChecks = map (\n v -> (n - r <= v, n + r >= v)) ns
-    searchKDT checks = search kdt $ cycle checks
-        where
-        search (Fork n a b) (f:fs) = case f n of
-            (True,True) -> search a fs `Set.union` search b fs
-            (True,False) -> search a fs
-            (False,True) -> search b fs
-            (False,False) -> Set.empty
-        search (Leaf xs) _ = Set.filter rangeFilter xs
-        search _ _ = Set.empty
+makeFrom :: (Floating n, Ord n, Ord a) => KDTree n a -> [a] -> KDTree n a
+makeFrom (KDTree getters radius _) xs = make radius getters xs
 
-nearbyMatching :: (Floating n, Ord n, Ord a) => KDT n a -> (a -> Bool) -> [a -> n] -> [n] -> n -> Set a
-nearbyMatching kdt predicate getters ns r = searchKDT leftRightChecks
+nearby :: (Floating n, Ord n, Ord a) => KDTree n a -> n -> [n] -> Set a
+nearby (KDTree getters _ kdt) r ns =
+        if length getters == length ns
+        then searchKDT leftRightChecks
+        else error "Search dimensions did not match KD-Tree dimensions."
     where
-    dims = length ns
-    fns = zip getters ns
-    rangeFilter a = sum (map (\(f,n) -> (f a-n)^(2::Int)) fns) <= r^dims
-    leftRightChecks = map (\n v -> (n - r <= v, n + r >= v)) ns
-    searchKDT checks = search kdt $ cycle checks
-        where
-        search (Fork n a b) (f:fs) = case f n of
-            (True,True) -> search a fs `Set.union` search b fs
-            (True,False) -> search a fs
-            (False,True) -> search b fs
-            (False,False) -> Set.empty
-        search (Leaf xs) _ = Set.filter (\a -> predicate a && rangeFilter a) xs
-        search _ _ = Set.empty
+        dims = length ns
+        fns = zip getters ns
+        rangeFilter a = sum (map (\(f,n) -> (f a-n)^(2::Int)) fns) <= r^dims
+        leftRightChecks = map (\n v -> (n - r <= v, n + r >= v)) ns
+        searchKDT checks = search kdt $ cycle checks
+            where
+            search (Fork n a b) (f:fs) = case f n of
+                (True,True) -> search a fs `Set.union` search b fs
+                (True,False) -> search a fs
+                (False,True) -> search b fs
+                (False,False) -> Set.empty
+            search (Leaf xs) _ = Set.filter rangeFilter xs
+            search _ _ = Set.empty
+
+nearbyMatching :: (Floating n, Ord n, Ord a) => KDTree n a -> (a -> Bool) -> n -> [n] -> Set a
+nearbyMatching (KDTree getters _ kdt) predicate r ns =
+        if length getters == length ns
+        then searchKDT leftRightChecks
+        else error "Search dimensions did not match KD-Tree dimensions."
+    where
+        dims = length ns
+        fns = zip getters ns
+        rangeFilter a = sum (map (\(f,n) -> (f a-n)^(2::Int)) fns) <= r^dims
+        leftRightChecks = map (\n v -> (n - r <= v, n + r >= v)) ns
+        searchKDT checks = search kdt $ cycle checks
+            where
+            search (Fork n a b) (f:fs) = case f n of
+                (True,True) -> search a fs `Set.union` search b fs
+                (True,False) -> search a fs
+                (False,True) -> search b fs
+                (False,False) -> Set.empty
+            search (Leaf xs) _ = Set.filter (\a -> predicate a && rangeFilter a) xs
+            search _ _ = Set.empty
